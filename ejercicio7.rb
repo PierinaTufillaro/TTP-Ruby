14d5113406ea3f42da928e2507aa686a8957452b
PierinaTufillaro <48775559+PierinaTufillaro@users.noreply.github.com>
2019-09-08 17:38:38 -0300

    Delete ejercicio7.rb

--- ejercicio7.rb
+++
@@ -1,9 +0,0 @@
-string = "Pierina Tufillaro"
-puts "-----Original-----"
-puts string
-puts "-----Reversa-----"
-puts string.reverse
-puts "-----Sin Espacios-----"
-puts string.gsub!(/\s+/, '')
-puts "-----Reemplazando con numeros-----"
-puts string.gsub!(/a+/,'4').gsub!(/e+/,'3').gsub!(/i+/,'1').gsub!(/o+/,'0').gsub!(/u+/,'6')

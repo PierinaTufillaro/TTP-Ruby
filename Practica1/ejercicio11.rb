13a1593f2cbd91ee885850aa3176f6a1f13d722d
pierinatufillaro <pierinatufillaro@hotmail.com>
2019-09-08 18:00:12 -0300

    a

--- Practica1/ejercicio11.rb
+++ Practica1/ejercicio11.rb
@@ -2,4 +2,4 @@ def rot13 (string)
 	string.tr("a-zA-Z","n-za-mN-ZA-M")
 end
 
-p rot("¡Bienvenidos a la cursada 2019 de TTPS Opción Ruby!")
+p rot13("¡Bienvenidos a la cursada 2019 de TTPS Opción Ruby!")

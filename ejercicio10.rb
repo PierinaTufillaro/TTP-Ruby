27b4f9631537127ef2e5914a44f4e1203b304754
PierinaTufillaro <48775559+PierinaTufillaro@users.noreply.github.com>
2019-09-08 17:37:54 -0300

    Delete ejercicio10.rb

--- ejercicio10.rb
+++
@@ -1,9 +0,0 @@
-def a_ul(aHash)
-	puts "<ul>"
-	aHash.each {|key, value| print "<li> #{key} : #{value} </l1>\n" }
-	puts "</ul>"
-end
-
-aH = {"perros:" => 1, "gatos:" => 1, "peces:" => 0 }
-
-a_ul(aH)

b522425763f60b83d57e9320ea3badd9a2a3e150
pierinatufillaro <pierinatufillaro@hotmail.com>
2019-09-17 16:44:48 -0300

    a

---
+++ Practica2/ejercicio3.rb
@@ -0,0 +1,13 @@
+def ordenar(num1,num2,num3)
+	arreglo = [num1,num2,num3]
+	arreglo.sort!
+	puts arreglo.to_s
+end
+
+entrada = [10, 9, 3]
+
+def ordenarArreglo (arreglo)
+	arreglo.each {|num| 
+	
+	}
+end
